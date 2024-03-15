e080aca2c272ac9b4bec84b231a283ebc929e667
@hexlet/cli <support@hexlet.io>
2024-03-15 13:40:00 +0300

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -5,7 +5,39 @@ require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
-  
+  def setup
+    @stack = Stack.new
+  end
+
+  def test_that_element_is_added
+    stack = @stack
+    stack.push!('test')
+    assert { stack.to_a == ['test'] }
+    assert { stack.size == 1 }
+    assert { !stack.empty? }
+  end
+
+  def test_that_element_is_deleted
+    stack = @stack
+    stack.push!('test_1')
+    stack.push!('test_2')
+    assert { stack.size == 2 }
+    stack.pop!
+    assert { stack.size == 1 }
+  end
+
+  def test_that_stack_is_cleared
+    stack = @stack
+    stack.push!('test')
+    assert { stack.empty? == false }
+    stack.clear!
+    assert { stack.empty? == true }
+  end
+
+  def test_that_stack_is_empty
+    stack = @stack
+    assert { stack.empty? }
+  end
   # END
 end
 
