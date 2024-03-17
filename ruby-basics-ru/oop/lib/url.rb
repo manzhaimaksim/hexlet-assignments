890dff3907ef59614c24bff3d71e90703b0120a1
manzhaimaksim <maxbbb88@gmail.com>
2024-03-17 13:01:04 +0300

    adding class Url

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -1,5 +1,51 @@
 # frozen_string_literal: true
 
 # BEGIN
+require 'uri'
 
+# class for work with URL
+class Url
+  include Comparable
+  extend Forwardable
+
+  def_delegators :scheme, :host, :port
+
+  attr_reader :uri
+
+  def initialize(params)
+    @uri = URI(params)
+  end
+
+  def scheme
+    @uri.scheme
+  end
+
+  def host
+    @uri.host
+  end
+
+  def port
+    @uri.port
+  end
+
+  def query_params
+    @uri.query.nil? ? {} : Hash[*@uri.query.split(/&|=/).flatten(1)].transform_keys(&:to_sym)
+  end
+
+  def query_param(key, *value)
+    if query_params.key?(key)
+      query_params[key]
+    elsif !value.empty?
+      value[0]
+    end
+  end
+
+  def <=>(other)
+    if !query_params.empty?
+      query_params <=> other.query_params && @uri.port <=> other.uri.port
+    else
+      @uri <=> other.uri
+    end
+  end
+end
 # END
